b53053eae4d2527e2cfc2aa473c952b2366625ac
coconutkim <achaeyeon18@gmail.com>
2024-10-15 10:20:11 +0900

    1015

---
+++ 1015_반복문.java
@@ -0,0 +1,13 @@
+// package fortest;
+
+public class fortest1{
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+			sum+=i;//sum=sum+i;
+		}
+		System.out.println('sum of 1~5');
+		System.out.println('sum of 1~5:%d\n')
+	}
+}
